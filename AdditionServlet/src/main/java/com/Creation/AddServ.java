e015cb9e258695efde5d12de4f8a6625dad5c298
Elvin <101944881+elvinmathew@users.noreply.github.com>
2024-01-24 17:21:59 +0530

    Add files via upload

---
+++ AdditionServlet/src/main/java/com/Creation/AddServ.java
@@ -0,0 +1,27 @@
+package com.Creation;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AddServ extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void service(HttpServletRequest req, HttpServletResponse res) throws IOException{
+	int a = Integer.parseInt(req.getParameter("num1"));
+	int b = Integer.parseInt(req.getParameter("num2"));
+	int c=a+b;
+	PrintWriter out=res.getWriter();
+	out.println(c);
+	
+}
+	}
+	
+
+
